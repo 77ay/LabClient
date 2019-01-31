ded1683bd6eef4cfcdf77ee6342f6b0d7e09b319
77ay <32190059+77ay@users.noreply.github.com>
2019-01-31 17:38:57 +0700

    Update
    Delete console.log(str) in palindromeCheck

--- Question7.js
+++ Question7.js
@@ -2,12 +2,11 @@ console.log('Palindrome')
 var status = false
 process.stdin.on('data', (a) => {
     var str = a.toString().trim()
-    console.log(palindromeCheck(str))
+    console.log("status :"+palindromeCheck(str))
 })
 var palindromeCheck = str =>{
     let mid = (str.length/2)-1
-    str = str.toLowerCase();
-    console.log(str)
+    str = str.toLowerCase()
     for(var i=mid+1, j=mid;i<str.length-1,j>=0;i++,j--){
         if (mid==isNaN()){
             if (str[i]==str[j]){
